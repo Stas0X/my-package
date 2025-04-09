dd39280dfdc82b57cc15a9538366b76049a84745
Stas0X <stasonchiku@gmail.com>
2025-04-09 10:57:16 +0200

    film2

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,39 @@
 import './App.css'
 
 function App() {
-  const a=5; m_komp=5
-  const b=33
+ 
 
 
  
   return (
     <>
-    
+    <link rel="icon" href="https://www.flaticon.com/free-icon/cinema_8363053" />
+    <title>SSFilms</title>
+    <body>
+      <h2 class="h0">Wyszukiwanie filmów</h2>
+    <div class="grid">
+      
+      <a href="https://www.youtube.com/watch?v=jan5CFWs9ic" target="_blank">
+      <img src="image/1.jpg"></img></a>
+      <a href="https://www.youtube.com/watch?v=82WW9dVbglI" target="_blank">
+      <img src="image/2.jpg"></img></a>
+      <a href="https://www.youtube.com/watch?v=vKQi3bBA1y8&t=7s" target="_blank">
+      <img src="image/3.jpg"></img></a>
+      <a href="https://www.youtube.com/watch?v=YDE97KrYDuU" target="_blank">
+      <img src="image/4.jpg"></img></a>
+      <a href="https://www.youtube.com/watch?v=pEO34SeTsY0" target="_blank">
+      <img src="image/5.jpg"></img></a>
+      <a href="https://www.youtube.com/watch?v=6COmYeLsz4c" target="_blank">
+      <img src="image/6.jpg"></img></a>
+      <a href="https://www.youtube.com/watch?v=cmSPwZIZu6Y" target="_blank">
+      <img src="image/7.jpg"></img></a>
+      <a href="https://www.youtube.com/watch?v=G5VLg5xnAUk" target="_blank">
+      <img src="image/8.jpg"></img></a>
+      <a href="https://www.youtube.com/watch?v=IhCEpkRmpc0" target="_blank">
+      <img src="image/9.jpg"></img></a>
+
+    </div>
+   </body>
     </>
   )
 }
