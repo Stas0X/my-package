78752d2a790cecec49d9b911755ef6f0c4b134ad
Stas0X <stasonchiku@gmail.com>
2025-04-28 10:57:31 +0200

    my-package0

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,30 @@ function App() {
       
   return (
     <>
-    <link rel="icon" href="https://www.flaticon.com/free-icon/cinema_8363053" />
+    <link rel="icon" href="https://cdn-icons-png.flaticon.com/128/3875/3875391.png" />
     <title>SSFilms</title>
     <body>
       <h2 class="h0">Wyszukiwanie filmów</h2>
       <div class="grid">
       
       <a href="https://www.youtube.com/watch?v=jan5CFWs9ic" target="_blank">
-      <img src="image/1.jpg"></img></a>
+      <img src="/image/1.jpg"></img></a>
       <a href="https://www.youtube.com/watch?v=82WW9dVbglI" target="_blank">
-      <img src="image/2.jpg"></img></a>
+      <img src="/image/2.jpg"></img></a>
       <a href="https://www.youtube.com/watch?v=vKQi3bBA1y8&t=7s" target="_blank">
-      <img src="image/3.jpg"></img></a>
+      <img src="/image/3.jpg"></img></a>
       <a href="https://www.youtube.com/watch?v=YDE97KrYDuU" target="_blank">
-      <img src="image/4.jpg"></img></a>
+      <img src="/image/4.jpg"></img></a>
       <a href="https://www.youtube.com/watch?v=pEO34SeTsY0" target="_blank">
-      <img src="image/5.jpg"></img></a>
+      <img src="/image/5.jpg"></img></a>
       <a href="https://www.youtube.com/watch?v=6COmYeLsz4c" target="_blank">
-      <img src="image/6.jpg"></img></a>
+      <img src="/image/6.jpg"></img></a>
       <a href="https://www.youtube.com/watch?v=cmSPwZIZu6Y" target="_blank">
-      <img src="image/7.jpg"></img></a>
+      <img src="/image/7.jpg"></img></a>
       <a href="https://www.youtube.com/watch?v=G5VLg5xnAUk" target="_blank">
-      <img src="image/8.jpg"></img></a>
+      <img src="/image/8.jpg"></img></a>
       <a href="https://www.youtube.com/watch?v=IhCEpkRmpc0" target="_blank">
-      <img src="image/9.jpg"></img></a>
+      <img src="/image/9.jpg"></img></a>
 
     </div>
     <img class="imgS" src="image/IconS.png" height="80px" width="80px"></img>
